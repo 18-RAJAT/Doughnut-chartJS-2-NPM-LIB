8cf6667974b214d1fb6607766c57f6092a18bfc4
18-RAJAT <Rajatjoshi7535@gmail.com>
2022-08-17 10:33:57 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import { Doughnut } from 'react-chartjs-2';
+import DoughnutChart from './DoughnutChart';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+    {/* <h1>Hello</h1> */}
+    <DoughnutChart/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/charts/Doughnut.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function DoughnutChar(){
+    return <div>
+        <h1>Doughnut</h1>
+    </div>;
+}
+
+export default DoughnutChar;
